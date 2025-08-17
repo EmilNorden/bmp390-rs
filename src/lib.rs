2f8b93fcc71a06e2facc389b6228e186a7da2df3
Emil Nordén <emilnorden@yahoo.se>
2025-08-17 19:49:19 +0200

    added some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -23,34 +23,37 @@ pub enum Bmp390Error<BusError> {
 
 type Bmp390Result<T, BusError> = Result<T, Bmp390Error<BusError>>;
 
+type Bmp390I2c<T> = Bmp390<I2c<T>>;
+type Bmp390Spi<T> = Bmp390<Spi<T>>;
+
 pub struct Bmp390<B> {
     bus: B,
     calibration_data: CalibrationData,
 }
 
-impl<I2cType> Bmp390<I2c<I2cType>>
+impl<T> Bmp390I2c<T>
 where
-    I2cType: embedded_hal_async::i2c::I2c,
-    I2c<I2cType>: Bus,
+    T: embedded_hal_async::i2c::I2c,
+    I2c<T>: Bus,
 {
     pub async fn new_i2c<D: DelayNs>(
-        i2c: I2cType,
+        i2c: T,
         address: SevenBitAddress,
         config: Configuration,
-        delay: &mut D) -> Bmp390Result<Self, <I2c<I2cType> as Bus>::Error> {
+        delay: &mut D) -> Bmp390Result<Self, <I2c<T> as Bus>::Error> {
         Self::new(I2c::new(i2c, address), config, delay).await
     }
 }
 
-impl<SpiType> Bmp390<Spi<SpiType>>
+impl<T> Bmp390Spi<T>
 where
-    SpiType: embedded_hal_async::spi::SpiDevice,
-    Spi<SpiType>: Bus,
+    T: embedded_hal_async::spi::SpiDevice,
+    Spi<T>: Bus,
 {
     pub async fn new_spi<D: DelayNs>(
-        spi: SpiType,
+        spi: T,
         config: Configuration,
-        delay: &mut D) -> Bmp390Result<Self, <Spi<SpiType> as Bus>::Error> {
+        delay: &mut D) -> Bmp390Result<Self, <Spi<T> as Bus>::Error> {
         Self::new(Spi::new(spi), config, delay).await
     }
 }
@@ -114,6 +117,17 @@ where
         Ok(buf[0] == BMP390_CHIP_ID)
     }
 
+    /// Sets the power mode of the device. 
+    /// 
+    /// As described in section 3.3.4 of the datasheet, these are the valid state transitions:
+    /// 
+    /// Sleep => Normal
+    /// 
+    /// Normal => Sleep
+    /// 
+    /// Sleep => Forced => Sleep (Forced is a transient state and the device will return to Sleep when the measurement is finished)
+    /// 
+    /// The device ignores any attempt to perform an invalid state transition.
     pub async fn set_mode(&mut self, mode: PowerMode) -> Bmp390Result<(), B::Error> {
         let mut buf = [0u8; 1];
         self.bus.read_register(Register::PwrCtrl, &mut buf)
@@ -128,6 +142,8 @@ where
         Ok(())
     }
 
+    /// Returns the current power mode.
+    /// 
     pub async fn mode(&mut self) -> Bmp390Result<PowerMode, B::Error> {
         let mut buf = [0u8; 1];
         self.bus.read_register(Register::PwrCtrl, &mut buf)
