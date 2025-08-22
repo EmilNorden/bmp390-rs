4334256221bae5628137b4160d8c6032d155ab44
Emil Nordén <emilnorden@yahoo.se>
2025-08-22 22:37:14 +0200

    Added functions to mask interrupts, and cleaned up API for getting status/interrupt status.

--- src/lib.rs
+++ src/lib.rs
@@ -11,33 +11,56 @@ pub mod testing;
 use crate::bus::{Bus, I2c, Spi};
 use crate::calibration::CalibrationData;
 use crate::config::Configuration;
-use crate::fifo::{ControlFrameType, FifoConfiguration, FifoFrame, FifoFullBehavior, FifoHeader, SensorFrameType};
+use crate::fifo::{
+    ControlFrameType, FifoConfiguration, FifoFrame, FifoFullBehavior, FifoHeader, SensorFrameType,
+};
+use crate::register::err_reg::ErrorFlags;
+use crate::register::fifo_config::{
+    FifoConfig1, FifoConfig1Fields, FifoConfig2, FifoConfig2Fields, FifoDataSource,
+};
 use crate::register::fifo_data::FifoData;
+use crate::register::fifo_length::FifoLength;
+use crate::register::int_ctrl::IntCtrl;
+use crate::register::int_status::{IntStatus, IntStatusFlags};
 use crate::register::{
-    InvalidRegisterField, Readable, Writable, chip_id, data, err_reg, odr, osr,
-    pwr_ctrl, status,
+    InvalidRegisterField, Readable, Writable, chip_id, data, err_reg, odr, osr, pwr_ctrl, status,
 };
 use core::fmt::Debug;
 use embedded_hal_async::delay::DelayNs;
 use embedded_hal_async::i2c::SevenBitAddress;
-use crate::register::fifo_config::{FifoConfig1, FifoConfig1Fields, FifoConfig2, FifoConfig2Fields, FifoDataSource};
-use crate::register::fifo_length::FifoLength;
 
 const BMP390_CHIP_ID: u8 = 0x60;
 
 #[derive(Debug)]
 pub enum Bmp390Error<BusError> {
+    /// An error has occurred in the SPI / I2C driver
     Bus(BusError),
+
+    /// Unable to communicate with BMP390
+    ///
+    /// Could possibly indicate an error with pin configuration and/or wiring.
     NotConnected,
+
+    /// Reading from a register returned unexpected data. This should not happen in normal circumstances.
+    ///
+    /// Could possibly indicate a bug in the driver, or less likely, a faulty chip or interference.
     UnexpectedRegisterData(InvalidRegisterField),
-    FormatError,
+
+    /// Reading from FIFO returned unexpected data. This should not happen in normal circumstances.
+    ///
+    /// Could possibly indicate a bug in the driver, or less likely, a faulty chip or interference.
+    UnexpectedFifoData,
 }
 
 type Bmp390Result<T, BusError> = Result<T, Bmp390Error<BusError>>;
 
+/// Type alias for a Bmp390 chip communicating over I2C
 type Bmp390I2c<T> = Bmp390<I2c<T>>;
+
+/// Type alias for a Bmp390 chip communicating over SPI
 type Bmp390Spi<T> = Bmp390<Spi<T>>;
 
+/// Main Bmp390 driver struct
 pub struct Bmp390<B> {
     bus: B,
     calibration_data: CalibrationData,
@@ -48,13 +71,38 @@ where
     T: embedded_hal_async::i2c::I2c,
     I2c<T>: Bus,
 {
+    /// Constructs a new Bmp390 driver instance with a given configuration that communicates over I2C
+    ///
+    /// This function will:
+    /// - Probe device for successful communication
+    /// - Apply the given configuration
+    /// - Load calibration coefficients from NVM
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # tokio_test::block_on(async {
+    /// # use embedded_hal_async::i2c::I2c as _;
+    /// use bmp390_rs::{Bmp390, SdoPinState};
+    /// use bmp390_rs::config::Configuration;
+    ///
+    /// let i2c = setup_i2c();
+    /// let mut delay = setup_delay();
+    ///
+    /// let device = Bmp390::new_i2c(
+    ///     i2c,
+    ///     SdoPinState::High,
+    ///     Configuration::default(),
+    ///     &mut delay
+    /// ).await.unwrap();
+    /// # });
     pub async fn new_i2c<D: DelayNs>(
         i2c: T,
-        address: SevenBitAddress,
+        sdo_pin_state: SdoPinState,
         config: Configuration,
         delay: &mut D,
     ) -> Bmp390Result<Self, <I2c<T> as Bus>::Error> {
-        Self::new(I2c::new(i2c, address), config, delay).await
+        Self::new(I2c::new(i2c, sdo_pin_state.into()), config, delay).await
     }
 }
 
@@ -148,14 +196,25 @@ where
 
     /// Returns the error flags from the ERR_REG (0x02) register.
     ///
-    /// These flags are
-    pub async fn error_flags(&mut self) -> Bmp390Result<err_reg::ErrorFlags, B::Error> {
-        Ok(self.bus.read::<err_reg::ErrReg>().await?)
+    /// **Note:** These flags are cleared upon read:
+    /// - cmd_err ([`Errors::command_execution_failed`])
+    /// - conf_err ([`Errors::configuration_error`])
+    pub async fn error_flags(&mut self) -> Bmp390Result<Errors, B::Error> {
+        Ok(Errors::from(self.bus.read::<err_reg::ErrReg>().await?))
     }
 
     /// Returns the status from the STATUS (0x03) register.
-    pub async fn status(&mut self) -> Bmp390Result<status::StatusFlags, B::Error> {
-        Ok(self.bus.read::<status::Status>().await?)
+    pub async fn status(&mut self) -> Bmp390Result<Status, B::Error> {
+        Ok(Status::from(self.bus.read::<status::Status>().await?))
+    }
+
+    /// Returns the interrupt status from the INT_STATUS (0x11) register.
+    ///
+    /// Use this to determine what caused an interrupt to be generated.
+    ///
+    /// **Note:** The INT_STATUS register is cleared upon read.
+    pub async fn interrupt_status(&mut self) -> Bmp390Result<InterruptStatus, B::Error> {
+        Ok(InterruptStatus::from(self.bus.read::<IntStatus>().await?))
     }
 
     /// Returns the current FIFO configuration. This is a combination of the fields stored in registers FIFO_CONFIG_1 (0x17) and FIFO_CONFIG_2 (0x18).
@@ -170,28 +229,43 @@ where
             config1.fifo_press_en,
             config1.fifo_temp_en,
             config1.fifo_time_en,
-            if config1.fifo_stop_on_full { FifoFullBehavior::Stop } else { FifoFullBehavior::OverwriteOldest },
+            if config1.fifo_stop_on_full {
+                FifoFullBehavior::Stop
+            } else {
+                FifoFullBehavior::OverwriteOldest
+            },
             config2.fifo_subsampling,
-            config2.data_select == FifoDataSource::Filtered
+            config2.data_select == FifoDataSource::Filtered,
         ))
     }
 
     /// Writes a new FIFO configuration to the device.
     ///
     /// This equates to updating registers FIFO_CONFIG_1 (0x17) and FIFO_CONFIG_2 (0x18)
-    pub async fn set_fifo_configuration(&mut self, cfg: FifoConfiguration) -> Bmp390Result<(), B::Error> {
-        self.bus.write::<FifoConfig1>(&FifoConfig1Fields {
-            fifo_mode: cfg.enable_fifo(),
-            fifo_stop_on_full: cfg.fifo_full_behavior() == FifoFullBehavior::Stop,
-            fifo_time_en: cfg.enable_time(),
-            fifo_press_en: cfg.enable_pressure(),
-            fifo_temp_en: cfg.enable_temperature(),
-        }).await?;
+    pub async fn set_fifo_configuration(
+        &mut self,
+        cfg: FifoConfiguration,
+    ) -> Bmp390Result<(), B::Error> {
+        self.bus
+            .write::<FifoConfig1>(&FifoConfig1Fields {
+                fifo_mode: cfg.enable_fifo(),
+                fifo_stop_on_full: cfg.fifo_full_behavior() == FifoFullBehavior::Stop,
+                fifo_time_en: cfg.enable_time(),
+                fifo_press_en: cfg.enable_pressure(),
+                fifo_temp_en: cfg.enable_temperature(),
+            })
+            .await?;
 
-        self.bus.write::<FifoConfig2>(&FifoConfig2Fields {
-            fifo_subsampling: cfg.subsampling() & 0b0000_0111,
-            data_select: if cfg.apply_iir_filter() { FifoDataSource::Filtered } else { FifoDataSource::Unfiltered },
-        }).await?;
+        self.bus
+            .write::<FifoConfig2>(&FifoConfig2Fields {
+                fifo_subsampling: cfg.subsampling() & 0b0000_0111,
+                data_select: if cfg.apply_iir_filter() {
+                    FifoDataSource::Filtered
+                } else {
+                    FifoDataSource::Unfiltered
+                },
+            })
+            .await?;
 
         Ok(())
     }
@@ -204,7 +278,7 @@ where
         Ok(self.bus.read::<FifoLength>().await?)
     }
 
-    /// Pops a frame from the top of the FIFO.
+    /// Pops a frame from the FIFO
     ///
     /// Note that the FIFO needs to be enabled in order for frames to be written to it, but it is still possible to read out frames even when the FIFO is disabled.
     /// Enabling the FIFO is done via [`Bmp390::set_fifo_configuration`].
@@ -264,7 +338,7 @@ where
             } else if header.config_change_flag() {
                 Ok(FifoFrame::ControlFrame(ControlFrameType::ConfigChange))
             } else {
-                Err(Bmp390Error::FormatError)
+                Err(Bmp390Error::UnexpectedFifoData)
             }
         } else if header.is_sensor_frame() {
             if header.sensor_time_flag() {
@@ -312,7 +386,7 @@ where
                 }
             }
         } else {
-            Err(Bmp390Error::FormatError)
+            Err(Bmp390Error::UnexpectedFifoData)
         }
     }
 
@@ -379,6 +453,220 @@ where
             pressure: compensated_pressure,
             temperature: compensated_temperature,
         })
+    }
+
+    /// Masks the given interrupts so they are not reflected on the INT pin
+    ///
+    /// The BMP390 exposes 3 interrupts:
+    /// - Data ready (DRDY)
+    /// - FIFO Full
+    /// - FIFO Watermark
+    ///
+    /// By masking any of these interrups you can control when the INT pin is activated.
+    ///
+    /// # Examples
+    /// ```rust, no_run
+    /// # tokio_test::block_on(async {
+    /// # let mut device = bmp390_rs::testing::dummy_device().await;
+    /// use bmp390_rs::Interrupts;
+    /// // Allow all interrupts
+    /// let interrupts = Interrupts::new().none();
+    /// device.mask_interrupts(interrupts).await.unwrap();
+    ///
+    /// // Mask FIFO full and FIFO watermark, leaving only Data ready.
+    /// let interrupts = Interrupts::new().fifo_full().fifo_watermark();
+    /// device.mask_interrupts(interrupts).await.unwrap();
+    /// # });
+    pub async fn mask_interrupts(&mut self, ints: Interrupts) -> Bmp390Result<(), B::Error> {
+        let mut int_ctrl = self.bus.read::<IntCtrl>().await?;
+
+        int_ctrl.drdy_en = !ints.is_data_ready_set();
+        int_ctrl.ffull_en = !ints.is_fifo_full_set();
+        int_ctrl.fwtm_en = !ints.is_fifo_watermark_set();
+        self.bus.write::<IntCtrl>(&int_ctrl).await?;
+
+        Ok(())
+    }
+}
+
+/// This enum should reflect the physical state of the SDO pin. This is used to determine the I2C address
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum SdoPinState {
+    /// SDO is pulled high by connection to VDDIO
+    High,
+    /// SDO is pulled low by connection to GND
+    Low,
+}
+
+impl Into<SevenBitAddress> for SdoPinState {
+    fn into(self) -> SevenBitAddress {
+        match self {
+            SdoPinState::High => 0x77,
+            SdoPinState::Low => 0x76,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Errors {
+    error_flags: ErrorFlags,
+}
+
+/// Holds information about any errors that has occurred.
+impl Errors {
+    /// A fatal error occurred.
+    pub fn fatal_error(&self) -> bool {
+        self.error_flags.fatal_err
+    }
+
+    /// Command execution failed.
+    ///
+    /// This value is cleared on **register** read.
+    pub fn command_execution_failed(&self) -> bool {
+        self.error_flags.cmd_err
+    }
+
+    /// Sensor configuration error detected.
+    ///
+    /// This can only happen in [`Normal´] power mode.
+    /// This value is cleared on **register** read.
+    pub fn configuration_error(&self) -> bool {
+        self.error_flags.conf_err
+    }
+}
+
+impl From<ErrorFlags> for Errors {
+    fn from(error_flags: ErrorFlags) -> Self {
+        Errors { error_flags }
+    }
+}
+
+/// Holds status fields from the [`Status`] (0x03) register.
+#[derive(Copy, Clone, Debug)]
+pub struct Status {
+    status: status::StatusFlags,
+}
+
+impl Status {
+    /// Is the command decoder ready to accept a new command?
+    ///
+    /// [`false`] means that a command is already in progress.
+    pub fn command_ready(&self) -> bool {
+        self.status.cmd_rdy
+    }
+
+    /// Is there new pressure data to be read?
+    ///
+    /// This value is cleared when any of the pressure DATA registers are read.
+    pub fn pressure_data_ready(&self) -> bool {
+        self.status.drdy_press
+    }
+
+    /// Is there new temperature data to be read?
+    ///
+    /// This value is cleared when any of the temperature DATA registers are read.
+    pub fn temperature_data_ready(&self) -> bool {
+        self.status.drdy_temp
+    }
+}
+
+impl From<status::StatusFlags> for Status {
+    fn from(status: status::StatusFlags) -> Self {
+        Status { status }
+    }
+}
+
+/// Holds information about what interrupts have been asserted since the INT_STATUS register was last read.
+#[derive(Copy, Clone, Debug)]
+pub struct InterruptStatus {
+    status: IntStatusFlags,
+}
+
+impl InterruptStatus {
+    /// Has a FIFO watermark interrupt been asserted?
+    ///
+    /// This value is cleared on **register** read.
+    pub fn fifo_watermark(&self) -> bool {
+        self.status.fwm_int
+    }
+
+    /// Has a FIFO full interrupt been asserted?
+    ///
+    /// This value is cleared on **register** read.
+    pub fn fifo_full(&self) -> bool {
+        self.status.ffull_int
+    }
+
+    pub fn data_ready(&self) -> bool {
+        self.status.drdy
+    }
+}
+
+impl From<IntStatusFlags> for InterruptStatus {
+    fn from(status: IntStatusFlags) -> Self {
+        Self { status }
+    }
+}
+
+pub struct Interrupts(u8);
+
+impl Interrupts {
+    const FIFO_FULL: u8 = 1 << 0;
+    const FIFO_WATERMARK: u8 = 1 << 1;
+    const DATA_READY: u8 = 1 << 2;
+
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    /// Add the FIFO full interrupt to this set.
+    ///
+    /// The FIFO full interrupt is used to signal when the number of unread bytes in the FIFO is >= 504.
+    /// See datasheet section 3.7.5.2 for more information.
+    pub fn fifo_full(mut self) -> Self {
+        self.0 |= Self::FIFO_FULL;
+
+        self
+    }
+
+    /// Add the FIFO watermark interrupt to this set.
+    ///
+    /// The FIFO watermark interrupt is used to signal that the number of unread bytes in the FIFO has reached
+    /// a pre-set limit. This limit is set by using [`Bmp390::set_fifo_configuration`].
+    /// See datasheet section 3.7.5.1 for more information.
+    pub fn fifo_watermark(mut self) -> Self {
+        self.0 |= Self::FIFO_WATERMARK;
+
+        self
+    }
+
+    /// Add the data ready (DRDY) interrupt to this set.
+    ///
+    /// DRDY is used to signal that a pressure/temperature measurement has ended and results have been stored in data registers and FIFO.
+    /// See datasheet section 3.7.5.3 for more information.
+    pub fn data_ready(mut self) -> Self {
+        self.0 |= Self::DATA_READY;
+
+        self
+    }
+
+    /// Remove all interrupts from the set.
+    pub fn none(mut self) -> Self {
+        self.0 = 0;
+
+        self
+    }
+
+    pub(crate) fn is_data_ready_set(&self) -> bool {
+        self.0 & Self::DATA_READY != 0
+    }
+
+    pub(crate) fn is_fifo_watermark_set(&self) -> bool {
+        self.0 & Self::FIFO_WATERMARK != 0
+    }
+
+    pub(crate) fn is_fifo_full_set(&self) -> bool {
+        self.0 & Self::FIFO_FULL != 0
     }
 }
 
@@ -501,7 +789,10 @@ mod tests {
             .unwrap();
 
         let frame = device.read_fifo_frame().await.unwrap();
-        assert_eq!(FifoFrame::ControlFrame(ControlFrameType::ConfigError), frame);
+        assert_eq!(
+            FifoFrame::ControlFrame(ControlFrameType::ConfigError),
+            frame
+        );
     }
 
     #[tokio::test]
@@ -517,6 +808,9 @@ mod tests {
             .unwrap();
 
         let frame = device.read_fifo_frame().await.unwrap();
-        assert_eq!(FifoFrame::ControlFrame(ControlFrameType::ConfigChange), frame);
+        assert_eq!(
+            FifoFrame::ControlFrame(ControlFrameType::ConfigChange),
+            frame
+        );
     }
 }
